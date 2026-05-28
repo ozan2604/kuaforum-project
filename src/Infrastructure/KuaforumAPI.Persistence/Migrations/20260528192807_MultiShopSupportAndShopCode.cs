a26b23eedc91715151b44e0fd168aec0418f512b
ozan2604 <ozansami04@gmail.com>
2026-05-29 00:27:59 +0300

    migration hatası

--- src/Infrastructure/KuaforumAPI.Persistence/Migrations/20260528192807_MultiShopSupportAndShopCode.cs
+++ src/Infrastructure/KuaforumAPI.Persistence/Migrations/20260528192807_MultiShopSupportAndShopCode.cs
@@ -10,23 +10,20 @@ namespace KuaforumAPI.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "UQ_Shops_OwnerId",
-                table: "Shops");
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM sys.indexes WHERE name = 'UQ_Shops_OwnerId' AND object_id = OBJECT_ID('Shops'))
+                    DROP INDEX [UQ_Shops_OwnerId] ON [Shops];
+            ");
 
-            migrationBuilder.AddColumn<string>(
-                name: "Code",
-                table: "Shops",
-                type: "nvarchar(10)",
-                maxLength: 10,
-                nullable: true);
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.columns WHERE name = 'Code' AND object_id = OBJECT_ID('Shops'))
+                    ALTER TABLE [Shops] ADD [Code] nvarchar(10) NULL;
+            ");
 
-            migrationBuilder.CreateIndex(
-                name: "UQ_Shops_Code",
-                table: "Shops",
-                column: "Code",
-                unique: true,
-                filter: "[Code] IS NOT NULL");
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'UQ_Shops_Code' AND object_id = OBJECT_ID('Shops'))
+                    CREATE UNIQUE INDEX [UQ_Shops_Code] ON [Shops] ([Code]) WHERE [Code] IS NOT NULL;
+            ");
         }
 
         /// <inheritdoc />
